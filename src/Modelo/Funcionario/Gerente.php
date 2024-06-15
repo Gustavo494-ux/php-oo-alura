ecae4a812b4d223f056683231bfdd06cad400c0e
Gustavo Gama <gustavogama494@gmail.com>
2024-06-15 20:07:31 -0300

    modulo 5 aula 4

--- src/Modelo/Funcionario/Gerente.php
+++ src/Modelo/Funcionario/Gerente.php
@@ -4,10 +4,19 @@ namespace Alura\Banco\Modelo\Funcionario;
 
 
 
-class Gerente extends Funcionario
+use Alura\Banco\Modelo\Autenticavel;
+
+class Gerente extends Funcionario implements Autenticavel
 {
     public function calculaBonificacao(): float
     {
         return $this->recuperaSalario();
     }
+
+    public function podeAutenticar(string $senha): bool
+    {
+        return $senha === '1234';
+    }
+
+
 }
